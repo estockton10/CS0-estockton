0a73dde108e18b7dd2f11daf7d3f2b806839a86a
Emily Stockton <estockton10@gmail.com>
2020-01-28 13:01:52 -0700

    worked on lab 1

--- Assignments/Labs/ascii/main.py
+++ Assignments/Labs/ascii/main.py
@@ -6,5 +6,8 @@
      This program prints ascii graphics on to the common output       /console/monitor.
 """
 
-print('    |\\_/|       **********************         (\\_/)  ')
-print(" / @ @ \\      *           Lab 1        *           (='.'=) ")
+print(  ' |\_/|   ***************************  (\_/)')
+print(    "/ @ @ \  *    ASCII lab            *  (=.=)")  
+print('(> 0 <)  *    By:Emily Stockton    * (")_(") ') 
+print(' >>x<<   *    CSCI 110             *')   
+print (' / O \   ***************************')    
