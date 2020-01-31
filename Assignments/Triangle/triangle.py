24ad175edc70b2519ce4607f618d36dfa43a16f6
Emily Stockton <estockton10@gmail.com>
2020-01-31 12:05:17 -0700

    added trianngle.py file

---
+++ Assignments/Triangle/triangle.py
@@ -0,0 +1,9 @@
+# triangle.py- By: Date- 31 Jan 2020
+# this program calculates and displays the perimeter and area of a triangle given 3 sides
+
+# Prompt user for side a, side b, and side c
+# Store side a,b,c as variables
+# Calculate perimeter with a+c+c=p
+# Calculate area with formulae s=(a+b+c)/s
+# a=sqrt(s(s-a)(s-b)(s-c))
+# display output 
