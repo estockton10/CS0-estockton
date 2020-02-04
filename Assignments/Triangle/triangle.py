4795d792dd9740b201e1f9d193096d481bd987cd
Emily Stockton <estockton10@gmail.com>
2020-02-03 22:04:54 -0700

    added triangle test

--- Assignments/Triangle/triangle.py
+++ Assignments/Triangle/triangle.py
@@ -1,30 +1,40 @@
-# triangle.py- By: Date- 31 Jan 2020
-# this program calculates and displays the perimeter and area of a triangle given 3 sides
+""" triangle.py- By: Emily Stockton
+#  Date- 31 Jan 2020
+# This program calculates and displays the perimeter and area of a triangle given 3 sides
 
 #Algorithim Steps
 # 1. Prompt user for side a, side b, and side c
 # 2. Store side a,b,c as variables
-# 3. Calculate perimeter with a+c+c=p
-# 4. Calculate area with formulae s=(a+b+c)/s
+# 3. Calculate perimeter with a+c+c=perimeter
+# 4. Calculate area with formulae s=(a+b+c)/s &
 #     a=sqrt(s(s-a)(s-b)(s-c))
-# 5. display output 
-
+# 5. Determine if valid triangle- if area less than or equal to 0, it is not a valid triangle
+# 6. display output 
+"""
 #import math library 
 import math
 
-print('This program will compute the area and perimeter of a triangle')
-side_a=input('Enter side a')
-side_a=int(side_a)
+#promt user for side lengths
+print('This program will compute the area and perimeter of a triangle when given the length of 3 sides')
+a=float(input('Enter length of side a'))
 
-side_b=input('Enter side b')
-side_b=int(side_b)
 
-side_c=input('Enter side c')
-side_c=int(side_c)
+b=float(input('Enter length of side b'))
 
-Perimeter=side_a+side_b+side_c
+
+c=float(input('Enter length of side c'))
+
+#calculate perimeter & area
+Perimeter=a+b+c
 print('The perimeter is',Perimeter)
 s=Perimeter/2
 
-Area=math.sqrt(s*(s-side_a)*(s-side_b)*(s-side_c))
+Area=math.sqrt(s*(s-a)*(s-b)*(s-c))
 print('The area is',Area)
+
+
+ #determine if valid triangle
+if Area<=0:
+    print('These sides do not form a valid triangle')
+else:
+    print('This is a valid triangle')
