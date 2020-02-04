62ed843bd9f9c1f721134060a8b52c05c529cd6b
Emily Stockton <estockton10@gmail.com>
2020-02-04 12:07:10 -0700

    updated circle.py file

--- Assignments/Labs/circle/circle.py
+++ Assignments/Labs/circle/circle.py
@@ -2,8 +2,8 @@
 
 ###
 # Circle - Basic Math - Lab
-# Updated By: FIXME1
-# Date: FIXME2
+# Updated By: Emily Stockton
+# Date: 2 Feb 2020
 # CSCI 110
 #
 # This program prompts user to enter radius of a circle and calculates and displays area
@@ -29,8 +29,9 @@ radius = float(radius)  # convert string to float value
 area = math.pi * radius**2  # * is product and ** is power operator in Python
 
 # Step 4
-# FIXME3:
+# FIXME3: #FIXED#
 # calculate and store the circumference into a variable
+circumfrence= 2*3.14*radius
 
 # Display the calculated values with proper descriptions
 print("Radius of the circle = ",  radius)
@@ -38,7 +39,8 @@ print("Radius of the circle = ",  radius)
 # FIXME4: display area - already fixed for you! #fixed#
 print('Area of the circle = ', area)
 
-# FIXME5: display circumference
+# FIXME5: display circumference #FIXED#
+print('Circumfrence of the circle=',circumfrence)
 
 # Verify/Test your program if the calculated results are correct
 # Run and test your program with different values for radius
