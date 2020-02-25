2eddf604fbdd0429a1990f6cdd3a4e7374f52af9
Emily Stockton <estockton10@gmail.com>
2020-02-25 14:56:05 -0700

    completed functions lab and function assignment

--- Assignments/Functions/functions.py
+++ Assignments/Functions/functions.py
@@ -1,20 +1,120 @@
 #########################
 #File name: functions.py 
-#computes different numbers
+#This file takes two numbers and finds the sum,differecene,product,quotient,remainder 
+# and finds the largest of the two numbers
 #CSCI 110
 #Emily Stockton
 #Last modified- 6 Feb 2020
 ##########################
 
+"""Write program and programmer information at the top of the program as comments (5 points)
+Define a function that takes two numbers, adds two numbers and returns the sum (5 points)
+Define a function that takes two numbers, multiplies the two numbers and returns the product (5 points)
+Define a function that takes two numbers; divides the first number by the second and returns the quotient (5 points)
+Define a function that takes two numbers, subtracts the second from the first and returns the difference (5 points)
+Define a function that takes two numbers, finds and returns the remainder of the first number divided by the second (5 points)
+Define a function that takes two numbers, finds the first to the power of the second number and returns the value (5 points)
+Define a function that takes a number and returns the square-root of the number (5 points)
+Prompt the user to enter two numbers (5 points)
+Call all the functions passing those two entered numbers and print the calculated results with proper descriptions (15 points)
+Write 2 tests cases using assert statements to automatically test each function. (5*7 = 35 points)
+Update README.md file with the status of the project and self grade. (5 points)
+Bonus 10 points - Write a function that finds and returns the larger of two given numbers. Call function to test it with the same two numbers.
 """
-Algorithm steps
-1. 
-"""
+
+
+
 import math
 
+#Define addition function
 def add(num1,num2):
-    total=num1 +num2
-return total
+    return int(num1) +int(num2)
+    
 
+#define multiply function
+def multiply(num1,num2):
+    return int(num1) *int(num2)
+
+
+#define divide function
+def divide(num1,num2):
+    return float(num1)//float(num2)
+
+
+
+#define subtract function
+def subtract(num1,num2):
+    return float(num1)-float(num2)
+
+
+
+#define remainder
+def remainder(num1,num2):
+    return int(num1)%int(num2)
+
+
+
+#define power function
+def power(num1,num2):
+    return float(num1)**(num2)
+
+
+
+#define sqrt
+def sqrt(num2):
+    return (num2**0.5)
+
+
+
+#function to find larger of two numbers
+def greaterthan(num1,num2):
+    if num1<num2:
+       result= num2
+    
+    elif num1>num2:
+         result=num1
+    
+    else:
+        result='the numbers are eqaul'
+    
+    return result
+
+
+#test funcitons
+def testFunction():
+    assert (add(5,5)==10)
+    assert (add(5,3)==8)
+    assert (multiply(5,5)==25)
+    assert(multiply(5,3)==15)
+    assert(divide(6,2)==3)
+    assert(divide(8,4)==2)
+    assert(remainder(7,5)==2)
+    assert(remainder(5,4)==1)
+    assert(power(2,8)==256)
+    assert(power(2,2)==4)
+    assert(sqrt(9)==3)
+    assert(sqrt(16)==4)
+    assert(greaterthan(2,1)==2)
+    print('all test functions passed')
+
+#call test functions
+testFunction()
+
+
+
+#ask user for 2 numbers
+num1=float(input("Enter a number: "))
+num2=float(input("Enter a second number: "))
+
+#output results
+print('The sum of your numbers is:',add(num1,num2))
+print('The product of your numbers is:',multiply(num1,num2))
+print('The quotient of your numbers is:',divide(num1,num2))
+print('The difference of your numbers is:',subtract(num1,num2))
+print('The remainder of your numbers is:',remainder(num1,num2))
+print('first number to the power of the second number is:',power(num1,num2))
+print('The square root of your first number is:',sqrt(num1))
+print('The square root of your second number is:',sqrt(num2))
+print('The larger of the two numbers is:',greaterthan(num1,num2))
 
 
--- Labs/functions/function.py
+++ Labs/functions/function.py
@@ -1,34 +1,54 @@
 ###
 # Functions lab 
-# Updated by: 
-# Date: 4 Feb 2020
+# Updated by: Emily Stockton #fixed#
+# Date: 4 Feb 2020 #fixed#
 # CSCI 110
-#
+# Functions & variable scopes
 ##
 
 # assign Bill Gates to someName variable
-someName="Bill Gates"
+someName="Bill Gates" #global variable
 
 # assign my name to myName
-myName="Insert Name"
+myName="Emily" #fixed# global variable
 
 def printHello():
-    print("Hello World!")
+     """
+    function that prints Hello World.
+    doesn't take any argument and doesn't return a value
+    """
+     print("Hello World!")
 
 def printHelloTwice():
+    """
+     function that prints Hello World twice
+     calls printHello function twice
+    """
     printHello()
     printHello()
 
 def greetName(name):
+    """ 
+    function that takes one argument name and greets the name
+    """
     print("Hi there {}".format(name))
 
 def meetAndGreet():
+    """
+    function that greets someone
+    doesn't take any argument and doesn't return any value
+    """
     firstName=input("\nHey there! What is your first name?\n")
-    greetName(firstName)
+    greetName(firstName) #call greetName function
     print("My name is {}. Nice to meet you.".format(myName))
     print()
 
 def convertTime(seconds):
+    """
+    #FIXME - #fixed#
+    Define a function named convertTime that takes 1 integer argument called seconds.
+    The function converts and prints the seconds in hours, minute, and seconds formats.
+    """
     hour=seconds//3600
     m=seconds//60
     m=m%60
@@ -36,18 +56,48 @@ def convertTime(seconds):
     print("{} seconds = {} hours, {} minutes, {} seconds".format(seconds, hour, m, sec))
 
 def findSeconds(hours):
-    seconds=0
+    """
+    #FIXME - #fixed#
+    Define a function named findSeconds that takes hours as 1 integer argument.
+    The function converts hours into seconds and returns it
+    """
+    #convert hours into seconds and update seconds variable#fixed#
+    seconds=hours *3600
     return seconds
 
 def testFunctions():
-    assert(findSeconds(1)==3600)
-    print('bruh it worked')
+    #test1: 1 hour==3600 seconds
+    assert(findSeconds(1) ==3600)
+    # FIXME5 - write 3 more tests cases for findSeconds function#fixed#
+    #test2: 24 hours==86400 seconds
+    assert(findSeconds(24)==86400)
+    #test3:48 hours==172800 seconds
+    assert(findSeconds(48)==172800)
 
-printHello
+    print('all test cases passed')
+    
+# FIXME6: call printHello function - #fixed#
+printHello()
+
+# FIXME7 - call printHelloTwice function #fixed#
 printHelloTwice
 
+greetName(someName)  # calling function passing variable as argument
+greetName("Larry Page") # calling function passing literal value as argument
+
+# call meetAndGreet function
 meetAndGreet()
+
+# call function convertTime passing proper argument
 convertTime(60)
+
+# FIXME8 - Call converTime function passing 3600 as seconds #fixed#
 convertTime(3600)
+
+
+# FIXME9 - Call converTime function passing 3661 as seconds #fixed#
+convertTime(3661)
+
+#FIXME10-call test functions #fixed$
 testFunctions()
 
