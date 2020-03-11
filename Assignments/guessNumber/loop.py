2a5d6aa9b45b3cef06ee6f6c25f6366e6e208dfd
Emily <estockton10@gmail.com>
2020-03-10 20:27:38 -0600

    added loop.py assignment

---
+++ Assignments/guessNumber/loop.py
@@ -0,0 +1,61 @@
+"""
+Assignment-Guess the number game
+This file lets user guess a number between 1 and 20 in 6 attempts
+CSCI 110
+By- Emily Stockton
+"""
+ 
+  
+   
+import random 
+number = random.randint(1, 5) 
+##CHANGE TO 1,20!!!!
+
+  
+def guess_number(): 
+    print("Welcome to guess a number game!")
+    name=input("What is your name?") 
+    print("Hello",name)
+    print("Guess a number (between 1 and 20), you have 6 tries:") 
+    chances = 0 
+    while chances < 6: 
+        
+        # Enter a number between 1 to 20 
+        guess = int(input()) 
+        
+        
+        if guess == number: 
+            
+            print("Congratulations", name)
+            print("You win!!!")
+            break
+            
+        
+        elif guess < number: 
+            print("Your guess was too low: Guess a number higher than", guess) 
+    
+                    
+        else: 
+            print("Your guess was too high: Guess a number lower than", guess) 
+            
+        
+        chances += 1
+            
+    if not chances < 6: 
+        print("YOU LOSE!!! The number is", number) 
+
+def main():
+    while True:
+        guess_number()
+
+        answer = input("Would you like to play again? [Y|N]")
+        if answer == 'y':
+            continue
+        elif answer=='Y':
+            continue
+        else:
+            break
+
+
+main()
+
