5e0b82e80c75df610e2d0a9316e1430ab98df476
Emily Stockton <estockton10@gmail.com>
2020-04-13 10:29:37 -0600

    added avion.py

---
+++ Assignments/avian.py/avian.py
@@ -0,0 +1,21 @@
+"""
+avion.py
+This file is for the kattis problem Avion
+CSCI 110
+Author- Emily Stockton
+12 April 2020
+"""
+
+codes= []
+
+for _ in range(5):
+    codes.append(input())
+
+found=False 
+for i in range(5):
+    if "FBI" in codes[i]:
+        print (i+1, end=' ')
+        found=True
+
+if not found:
+        print('HE GOT AWAY!')
