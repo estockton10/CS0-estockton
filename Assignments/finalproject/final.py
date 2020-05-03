a33b06eb0bd10d124035d55feaa2b75c8e2750d6
Emily Stockton <estockton10@gmail.com>
2020-05-03 14:17:25 -0600

    added word.txt, updated final.py

--- Assignments/finalproject/final.py
+++ Assignments/finalproject/final.py
@@ -7,3 +7,81 @@ Author-
 
 """
 
+import random
+
+
+def get_words():
+    
+
+def gallows():
+    if guess_num==0:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|    ')  
+        print('|') 
+        print('|   ')   
+        print('|') 
+        print('|   ')                             
+        print('-------------')
+
+    elif guess_num==1:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|          0')  
+        print('|           ' )
+        print('|          ')
+        print('|         ' )  
+        print('|') 
+        print('|   ')                             
+        print('-------------')
+
+    elif guess_num==2:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|          0')  
+        print('|          | ' )
+        print('|           ')
+        print('|           ' )  
+        print('|') 
+        print('|   ')                             
+        print('-------------')
+
+    elif guess_num==3:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|          0')  
+        print('|         /|  ' )
+        print('|           ' )  
+        print('|') 
+        print('|   ')                             
+        print('-------------')
+
+    elif guess_num==4:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|          0')  
+        print('|         /|\  ' )
+        print('|           ' )  
+        print('|') 
+        print('|   ')                             
+        print('-------------')
+
+    elif guess_num==5:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|          0')  
+        print('|         /|\  ' )
+        print('|         /   ' )  
+        print('|') 
+        print('|   ')                             
+        print('-------------')
+
+    elif guess_num==6:
+        print('|__________ '  )                      
+        print('|/         |') 
+        print('|          0')  
+        print('|         /|\  ' )
+        print('|         / \  ' )  
+        print('|') 
+        print('|   ')                             
+        print('-------------') 
